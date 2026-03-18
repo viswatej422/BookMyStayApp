c6bb1de61871fe89243c6f1b3e7210789bf73633
viswatej422 <viswatej422@gmail.com>
2026-03-18 10:30:54 +0530

    use case -3 adding single room and suite room

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,13 +1,29 @@
-class HotelBookingApp{
+import java.util.Map;
+
+public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        System.out.println("=====================================");
-        System.out.println("   Welcome to Book My Stay App");
-        System.out.println("   Hotel Booking System v1.0");
-        System.out.println("=====================================");
+        System.out.println("===== Book My Stay App =====");
+        System.out.println("Hotel Room Inventory Status\n");
 
-        System.out.println("Application started successfully.");
-        System.out.println("Application execution completed.");
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        RoomInventory inventory = new RoomInventory();
+        Map<String, Integer> availability = inventory.getRoomAvailability();
+
+        System.out.println("Single Room:");
+        single.displayRoomDetails();
+        System.out.println("Available Rooms: " + availability.get("Single"));
+
+        System.out.println("\nDouble Room:");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + availability.get("Double"));
+
+        System.out.println("\nSuite Room:");
+        suite.displayRoomDetails();
+        System.out.println("Available Rooms: " + availability.get("Suite"));
     }
 }
